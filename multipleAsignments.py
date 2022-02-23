e1bf62cd8fbdc7342028760598fcd55909373a75
AlisonGurgel <100238054+AlisonGurgel@users.noreply.github.com>
2022-02-23 11:05:58 -0300

    Create multipleAsignments.py

---
+++ multipleAsignments.py
@@ -0,0 +1,16 @@
+x = 0.0
+y = 0.0
+z = 0.0 
+print(x, y, z)
+
+x = y = z = 2.0
+print(x, y, z)
+
+a = "lets"
+b = "test"
+c = "asignments"
+print(a, b, c)
+
+a, b, c = "lets", "test", "asignment"
+print(a, b, c)
+
